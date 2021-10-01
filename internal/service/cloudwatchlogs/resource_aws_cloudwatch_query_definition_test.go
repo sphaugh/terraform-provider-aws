f31414ec3a18e46a1700b58955f2df8fd7917ff1
Dirk Avery <dirk.avery@gmail.com>
2021-10-01 19:59:34 -0400

    cloudwatchlogs: Export/un-export after move

--- internal/service/cloudwatchlogs/resource_aws_cloudwatch_query_definition_test.go
+++ internal/service/cloudwatchlogs/resource_aws_cloudwatch_query_definition_test.go
@@ -239,7 +239,7 @@ func testAccCheckAWSCloudWatchQueryDefinitionExists(rName string, v *cloudwatchl
 		}
 		conn := acctest.Provider.Meta().(*conns.AWSClient).CloudWatchLogsConn
 
-		result, err := finder.QueryDefinition(context.Background(), conn, "", rs.Primary.ID)
+		result, err := finder.FindQueryDefinition(context.Background(), conn, "", rs.Primary.ID)
 
 		if err != nil {
 			return err
@@ -263,7 +263,7 @@ func testAccCheckAWSCloudWatchQueryDefinitionDestroy(s *terraform.State) error {
 			continue
 		}
 
-		result, err := finder.QueryDefinition(context.Background(), conn, "", rs.Primary.ID)
+		result, err := finder.FindQueryDefinition(context.Background(), conn, "", rs.Primary.ID)
 		if err != nil {
 			return fmt.Errorf("error reading CloudWatch query definition (%s): %w", rs.Primary.ID, err)
 		}
