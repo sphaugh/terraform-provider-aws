99e3aa56f244dbda3116d9d2632ababd3842797c
Dirk Avery <dirk.avery@gmail.com>
2021-10-01 20:04:00 -0400

    workspaces: Fix many import problems

--- internal/service/workspaces/resource_aws_workspaces_ip_group_test.go
+++ internal/service/workspaces/resource_aws_workspaces_ip_group_test.go
@@ -1,4 +1,4 @@
-package aws
+package workspaces_test
 
 import (
 	"fmt"
@@ -11,13 +11,13 @@ import (
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/workspaces/lister"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	tfworkspaces "github.com/hashicorp/terraform-provider-aws/internal/service/workspaces"
 )
 
 func init() {
@@ -36,7 +36,7 @@ func testSweepWorkspacesIpGroups(region string) error {
 	input := &workspaces.DescribeIpGroupsInput{}
 	sweepResources := make([]*sweep.SweepResource, 0)
 
-	err = lister.DescribeIPGroupsPages(conn, input, func(page *workspaces.DescribeIpGroupsOutput, lastPage bool) bool {
+	err = tfworkspaces.DescribeIPGroupsPages(conn, input, func(page *workspaces.DescribeIpGroupsOutput, lastPage bool) bool {
 		if page == nil {
 			return !lastPage
 		}
