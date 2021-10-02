e3cafe6fda126cc6c20d7e4bee3434609f8a8412
Dirk Avery <dirk.avery@gmail.com>
2021-10-01 20:03:40 -0400

    directconnect: Fix many import problems

--- internal/service/directconnect/resource_aws_dx_lag_test.go
+++ internal/service/directconnect/resource_aws_dx_lag_test.go
@@ -1,4 +1,4 @@
-package aws
+package directconnect_test
 
 import (
 	"fmt"
@@ -12,14 +12,14 @@ import (
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/directconnect/finder"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	tfdirectconnect "github.com/hashicorp/terraform-provider-aws/internal/service/directconnect"
 )
 
 func init() {
@@ -261,7 +261,7 @@ func testAccCheckAwsDxLagDestroy(s *terraform.State) error {
 			continue
 		}
 
-		_, err := finder.FindLagByID(conn, rs.Primary.ID)
+		_, err := tfdirectconnect.FindLagByID(conn, rs.Primary.ID)
 
 		if tfresource.NotFound(err) {
 			continue
@@ -290,7 +290,7 @@ func testAccCheckAwsDxLagExists(name string, v *directconnect.Lag) resource.Test
 			return fmt.Errorf("No ID is set")
 		}
 
-		lag, err := finder.FindLagByID(conn, rs.Primary.ID)
+		lag, err := tfdirectconnect.FindLagByID(conn, rs.Primary.ID)
 
 		if err != nil {
 			return err
