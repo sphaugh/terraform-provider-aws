28b2925d0e2f4cd4445f72138c87dd8521bd802b
Dirk Avery <dirk.avery@gmail.com>
2021-10-01 20:03:36 -0400

    budgets: Fix many import problems

--- internal/service/budgets/resource_aws_budgets_budget_test.go
+++ internal/service/budgets/resource_aws_budgets_budget_test.go
@@ -1,4 +1,4 @@
-package aws
+package budgets_test
 
 import (
 	"fmt"
@@ -14,15 +14,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
-	tfbudgets "github.com/hashicorp/terraform-provider-aws/aws/internal/service/budgets"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/service/budgets/finder"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	tfbudgets "github.com/hashicorp/terraform-provider-aws/internal/service/budgets"
+	tfbudgets "github.com/hashicorp/terraform-provider-aws/internal/service/budgets"
 )
 
 func init() {
@@ -429,7 +429,7 @@ func testAccAWSBudgetsBudgetExists(resourceName string, v *budgets.Budget) resou
 			return err
 		}
 
-		output, err := finder.FindBudgetByAccountIDAndBudgetName(conn, accountID, budgetName)
+		output, err := tfbudgets.FindBudgetByAccountIDAndBudgetName(conn, accountID, budgetName)
 
 		if err != nil {
 			return err
@@ -455,7 +455,7 @@ func testAccAWSBudgetsBudgetDestroy(s *terraform.State) error {
 			return err
 		}
 
-		_, err = finder.FindBudgetByAccountIDAndBudgetName(conn, accountID, budgetName)
+		_, err = tfbudgets.FindBudgetByAccountIDAndBudgetName(conn, accountID, budgetName)
 
 		if tfresource.NotFound(err) {
 			continue
