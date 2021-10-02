a423c467612e1409c79e6414d6962e55ef542372
Dirk Avery <dirk.avery@gmail.com>
2021-10-01 20:08:22 -0400

    mq: Fix more import problems

--- internal/service/mq/broker_test.go
+++ internal/service/mq/broker_test.go
@@ -18,11 +18,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	"github.com/hashicorp/terraform-provider-aws/internal/provider"
-	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
-	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
-	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	tfmq "github.com/hashicorp/terraform-provider-aws/internal/service/mq"
+	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
 func init() {
